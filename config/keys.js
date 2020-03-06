33353a15fd89745e6b5a1d129f605373406d6c84
Zhengyang Zhao <mason.zh@gmail.com>
2020-03-06 08:49:49 -0600

    add some views

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://Zhengyang:Zzy1229!@ds263808.mlab.com:63808/multifunctionweb-dev',
-    googleClientID: '505511999300-dlrjbd5ibn8vltootqllova3l3auvdsp.apps.googleusercontent.com',
-    googleClientSecret: 'g6QeKQaHXu_Cd1RHXZeqoa-e'
-}
